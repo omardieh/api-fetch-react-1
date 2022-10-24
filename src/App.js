fb83ecaa731d640e406c3d45b16317e61eafb826
omardiehs <omar.dieh@wildecodeschool.com>
2022-10-24 12:37:41 +0200

    api fetch axios with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [apiResp, setApiResp] = useState([]);
+  // const [pokemonId, setPokemonId] = useState(1);
+  const apiUrl = "https://fakestoreapi.com/products";
+  // const pokemonApiWithId = `https://pokeapi.co/api/v2/berry/${pokemonId}`
+
+  // get request using fetch function :
+  // async function handleFetch (url,setResp) {
+  //   fetch(url).then((resp)=>resp.json()).then((data)=>setResp(data))
+  // }
+
+  // get request using axios function :
+  async function handleFetch (url,setResp) {
+    axios.get(url).then((data)=>setResp(data.data));
+  }
+  
+  
+  useEffect(()=>{
+    handleFetch(apiUrl,setApiResp)
+  },[])
+  
+  // async function handleFetch (url,setResp) {
+  //   axios.get(url).then((data)=>setResp(data.data));
+  // }
+
+  // useEffect(()=>{
+  //   handleFetch(pokemonApiWithId,setApiResp)
+  // },[pokemonId])
+  
+  console.log(apiResp)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {apiResp.map((element,index)=><ul key={index}>
+        <li> {element.title} </li>
+        <img src={element.image} />
+      </ul>)}
     </div>
   );
 }
